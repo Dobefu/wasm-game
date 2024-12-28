4c7288755317169af5b1093f7242e718ed83618f
Connor van Spronssen <info@dobefu.com>
2024-12-28 16:41:04 +0100

    feat: Add a delta time

--- wasm/cmd/game/main.go
+++ wasm/cmd/game/main.go
@@ -1,31 +1,40 @@
 package game
 
 import (
+	"fmt"
 	"math"
+	"time"
 
 	"github.com/Dobefu/wasm-game/cmd/canvas"
 )
 
 var (
-	CANVAS *canvas.Canvas
-	frame  int64
+	CANVAS     *canvas.Canvas
+	FRAME      int
+	DELTA_TIME int64
+	_LAST_TIME time.Time
 )
 
 func init() {
 	CANVAS = &canvas.CANVAS
+	_LAST_TIME = time.Now()
 }
 
 func Update() {
-	frame = frame + 1
+	FRAME = FRAME + 1
 }
 
 func Render(clearCanvas bool) {
+	DELTA_TIME = time.Now().UnixMilli() - _LAST_TIME.UnixMilli()
+	_LAST_TIME = time.Now()
+	fmt.Println(DELTA_TIME)
+
 	if clearCanvas {
 		CANVAS.Context.Call("clearRect", 0, 0, CANVAS.Width, CANVAS.Height)
 	}
 
-	sin := math.Sin(float64(frame) / 30)
-	cos := math.Cos(float64(frame) / 30)
+	sin := math.Sin(float64(FRAME) / 30)
+	cos := math.Cos(float64(FRAME) / 30)
 
 	CANVAS.Context.Call("beginPath")
 	CANVAS.Context.Call("moveTo", (float64(CANVAS.Width/2)*cos)+(float64(CANVAS.Width)/2), (float64(CANVAS.Height/2)*sin)+(float64(CANVAS.Height)/2))
