89d5978df8e678d8582c6db1e8eb4621b36ef616
Connor van Spronssen <info@dobefu.com>
2024-12-28 13:51:49 +0100

    feat: Move the WASM loading logic to the main script

--- js/main.js
+++ js/main.js
@@ -1,4 +1,13 @@
 'use strict'
 
 // @ts-check
-;(() => {})()
+;(() => {
+  const go = new Go()
+
+  WebAssembly.instantiateStreaming(
+    fetch('wasm/game.wasm'),
+    go.importObject,
+  ).then((result) => {
+    go.run(result.instance)
+  })
+})()
