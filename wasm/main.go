cf2075b327250b6561cb575f467f5818306f6193
Connor van Spronssen <info@dobefu.com>
2024-12-28 13:49:14 +0100

    feat: Add a basic line to the canvas

--- wasm/main.go
+++ wasm/main.go
@@ -1,7 +1,27 @@
+//go:build js && wasm
+// +build js,wasm
+
 package main
 
-import "fmt"
+import (
+	"syscall/js"
+)
+
+func render() {
+	var canvas js.Value = js.Global().Get("document").Call("getElementById", "game")
+	var context js.Value = canvas.Call("getContext", "2d")
+
+	canvas.Set("height", 100)
+	canvas.Set("width", 100)
+
+	for i := 0; i < 50; i++ {
+		context.Call("beginPath")
+		context.Call("moveTo", 0, 0)
+		context.Call("lineTo", 100, 100)
+		context.Call("stroke")
+	}
+}
 
 func main() {
-	fmt.Println("Main")
+	render()
 }
