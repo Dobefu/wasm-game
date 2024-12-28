19463b4ca537f377b22ee39cb76cdd989bed49a7
Connor van Spronssen <info@dobefu.com>
2024-12-28 16:54:34 +0100

    feat: Apply the delta time, to ensure consistent speeds across framerates

--- wasm/cmd/game/main.go
+++ wasm/cmd/game/main.go
@@ -1,7 +1,6 @@
 package game
 
 import (
-	"fmt"
 	"math"
 	"time"
 
@@ -11,8 +10,9 @@ import (
 var (
 	CANVAS     *canvas.Canvas
 	FRAME      int
-	DELTA_TIME int64
+	DELTA_TIME float64
 	_LAST_TIME time.Time
+	ROTATION   float64
 )
 
 func init() {
@@ -25,16 +25,16 @@ func Update() {
 }
 
 func Render(clearCanvas bool) {
-	DELTA_TIME = time.Now().UnixMilli() - _LAST_TIME.UnixMilli()
+	DELTA_TIME = float64(time.Now().UnixMilli()-_LAST_TIME.UnixMilli()) / 1000
 	_LAST_TIME = time.Now()
-	fmt.Println(DELTA_TIME)
 
 	if clearCanvas {
 		CANVAS.Context.Call("clearRect", 0, 0, CANVAS.Width, CANVAS.Height)
 	}
 
-	sin := math.Sin(float64(FRAME) / 30)
-	cos := math.Cos(float64(FRAME) / 30)
+	ROTATION += DELTA_TIME
+	sin := math.Sin(ROTATION)
+	cos := math.Cos(ROTATION)
 
 	CANVAS.Context.Call("beginPath")
 	CANVAS.Context.Call("moveTo", (float64(CANVAS.Width/2)*cos)+(float64(CANVAS.Width)/2), (float64(CANVAS.Height/2)*sin)+(float64(CANVAS.Height)/2))
