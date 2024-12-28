98375939acc52261e2522f510c60136218f3e91b
Connor van Spronssen <info@dobefu.com>
2024-12-28 13:50:43 +0100

    feat: Move the canvas definition to the HTML file

--- js/main.js
+++ js/main.js
@@ -1,17 +1,4 @@
 'use strict'
 
 // @ts-check
-
-function createCanvas() {
-  const canvas = document.createElement('canvas')
-  canvas.id = 'game'
-
-  document.body.appendChild(canvas)
-
-  return canvas
-}
-
-;(() => {
-  const canvas = createCanvas()
-  console.log(canvas)
-})()
+;(() => {})()
