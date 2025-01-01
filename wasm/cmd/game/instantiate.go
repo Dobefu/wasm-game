6d8e658fe7498f385cdc8ae94d0227e88a1d2f5c
Connor van Spronssen <info@dobefu.com>
2025-01-01 20:27:54 +0100

    feat: Make the Instantiate function return the game object

--- wasm/cmd/game/instantiate.go
+++ wasm/cmd/game/instantiate.go
@@ -2,6 +2,8 @@ package game
 
 import "github.com/Dobefu/wasm-game/cmd/game/structs"
 
-func Instantiate(gameObject structs.GameObject) {
+func Instantiate(gameObject structs.GameObject) *structs.GameObject {
 	GAME_OBJECTS = append(GAME_OBJECTS, &gameObject)
+
+	return &gameObject
 }
