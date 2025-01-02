f584d08271938dc904c4f5ea9094caec5ebfcb61
Connor van Spronssen <info@dobefu.com>
2025-01-02 13:21:09 +0100

    feat: Add a RotatePoint function

---
+++ wasm/cmd/rotate/rotate-point.go
@@ -0,0 +1,11 @@
+package rotate
+
+import (
+	"math"
+)
+
+func RotatePoint(x, y, cx, cy, angle float64) (float64, float64) {
+	s, c := math.Sincos(angle)
+	dx, dy := x-cx, y-cy
+	return cx + (dx*c - dy*s), cy + (dx*s + dy*c)
+}
