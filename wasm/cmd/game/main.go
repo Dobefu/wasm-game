3516fba6c8704221d5bbea8529414c6d87d75175
Connor van Spronssen <info@dobefu.com>
2025-01-02 14:20:01 +0100

    feat: Add some basic level geometry

--- wasm/cmd/game/main.go
+++ wasm/cmd/game/main.go
@@ -18,4 +18,11 @@ var (
 func init() {
 	CANVAS = &canvas.CANVAS
 	_LAST_TIME = time.Now()
+
+	Instantiate(structs.GameObject{Width: 10000, Height: 10, X: 0, Y: 800})
+	Instantiate(structs.GameObject{Width: 50, Height: 50, X: 200, Y: 770})
+	Instantiate(structs.GameObject{Width: 50, Height: 50, X: 250, Y: 770})
+	Instantiate(structs.GameObject{Width: 50, Height: 50, X: 450, Y: 770})
+	Instantiate(structs.GameObject{Width: 50, Height: 50, X: 550, Y: 770})
+	Instantiate(structs.GameObject{Width: 50, Height: 50, X: 350, Y: 720})
 }
